366eb9eabb7ee35fce80ed7464f31f60d2052559
Z-Walt-734 <43279092+Z-Walt-734@users.noreply.github.com>
2020-04-29 17:32:12 -0500

    Add files via upload

--- alu.sv
+++ alu.sv
@@ -1,55 +1,20 @@
-
-//Creates module 
-
-module alu(A,b,F,y,Z); 
-
-//Declares inputs and outputs of the circuit 
-// Cleaned up to be verilog instead of systemverilog
-input [31:0] A,b; 
-
-input [2:0] F; 
-
-output logic [31:0] y; 
-
-output Z; 
-
-logic [31:0] sum, alu_out;  
-
-//Assigns other ports that will make the circuit work properly                    
-
-assign alu_out = F[2]?~b:b;                                                                                                                                           
-
-assign sum =  A + alu_out + F[2];                                                                                                                                   
-
-always@(*) 
-
-//Gives different states of the circuit based on what value it is on 
-
-	case(F[1:0])       
-	//removed F[2] since not neccessary
-	// removed second half of case since redundant                                                                                                                                
-
-		3'b00:y=A&alu_out;                                                                                                                         
-
-		3'b01:y=A|alu_out;                                                                                                                                          
-
-		3'b10:y=sum;                                                                                                                                                 
-
-		3'b11:y=sum[31];  
-	  
-
-	//Terminates case statement                                                                                                                                                
-
-	endcase 
-
-assign Z=(y==0);// if y is 0 Z=1(true), y is 1 Z=0(false) 
-
-//terminates module 
-
-endmodule 
-
-
-
- 
-
- 
+module alu (A, B, F, Y, Zero);
+	input [31:0] A, B;
+	input [2:0] F;
+	output reg [31:0] Y;
+	output Zero;
+	wire [31:0] SUM, Bout;
+	
+	assign Bout = F[2]? ~B: B;
+		
+	assign SUM = A + Bout + F[2];
+	assign Zero = (Y == 0);
+	
+	always @(*)
+		case(F[1:0])
+			2'b00: Y = A & Bout;
+			2'b01: Y = A | Bout;
+			2'b10: Y = SUM;
+			2'b11: Y = SUM[31];
+		endcase
+endmodule
--- testbench.sv
+++ testbench.sv
@@ -1,8 +1,8 @@
-module testbench ();
+module testbench(output logic b);
 	logic clk, reset, mem_write;
+	
 
 	logic [31:0] write_data, data_addr;
-
 	top dut(clk, reset, write_data, data_addr, mem_write);
 
 	initial begin
@@ -13,17 +13,20 @@ module testbench ();
 	always begin
 		clk <= 1; #5;
 		clk <= 0; #5;
-	end
 
-	always@(negedge clk)begin
+	end
+	
+		assign b = clk;
+
+	always@(posedge clk)begin
 		if(mem_write) begin
 			if(data_addr === 84 & write_data === 7)begin
 				$display("Success");
-				$stop;
+
 			end
 			else if(data_addr !== 80)begin
 				$display("Error");
-				$stop;
+
 			end
 		end
 	end
